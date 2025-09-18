8bb60372eb0f26ac99d4fcba6bc5810bc812aeeb
CesarAle31 <l22920168@voaxaca.tecnm.mx>
2025-09-18 11:51:11 -0600

    agregue codigo

--- scripts/cesar.js
+++ scripts/cesar.js
@@ -9,3 +9,18 @@ rl.question("¿Cómo te llamas? ", function(nombre) {
     console.log(`¡Hola, ${nombre}!`);
     rl.close();
 });
+
+
+
+// Función para determinar si un número es par o impar
+function esParOImpar(numero) {
+    if (numero % 2 === 0) {
+        console.log(`${numero} es un número par.`);
+    } else {
+        console.log(`${numero} es un número impar.`);
+    }
+}
+
+// Ejemplo de uso
+esParOImpar(7);  // Output: 7 es un número impar.
+esParOImpar(12); // Output: 12 es un número par.
